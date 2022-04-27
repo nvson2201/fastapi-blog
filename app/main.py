123c615774fe848dd4aef2676baaaed55eaceaa0
Son Nguyen <n.vanson2201@gmail.com>
2022-04-27 17:22:22 +0700

    fix

--- app/main.py
+++ app/main.py
@@ -12,18 +12,3 @@ from app.redis.containers import Container
 app = FastAPI()
 app.include_router(api_router, prefix=settings.API_V1_STR)
 app.add_middleware(SessionMiddleware, secret_key="!secret")
-
-
-@app.get("/")
-@inject
-async def index(service: Service = Depends(Provide[Container.service])):
-    value = await service.process()
-    return {"result": value}
-
-
-container = Container()
-container.config.redis_host.from_env("REDIS_HOST", "localhost:6379")
-container.config.redis_password.from_env("REDIS_PASSWORD", "123456")
-container.wire(modules=[__name__])
-
-app.include_router(api_router, prefix=settings.API_V1_STR)
