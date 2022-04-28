e660ca8a44d0b14d6ffb96004611536bed104c83
Son Nguyen <n.vanson2201@gmail.com>
2022-04-28 11:13:50 +0700

    split table to separate file - migrate

---
+++ migrations/versions/a4df00aacad7_adding_comments_table.py
@@ -0,0 +1,38 @@
+"""Adding Comments Table
+
+Revision ID: a4df00aacad7
+Revises:
+Create Date: 2022-04-16 22:49:04.121505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a4df00aacad7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # create comments table
+    op.create_table(
+        'comment',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('body', sa.String(length=255), nullable=True),
+        sa.Column('contain_id', sa.Integer(), nullable=True),
+        sa.Column('owner_id', sa.Integer(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ['contain_id'], ['post.id'], ondelete='CASCADE'),
+        sa.ForeignKeyConstraint(
+            ['owner_id'], ['user.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_comment_id'), 'comment', ['id'], unique=False)
+
+
+def downgrade():
+    op.drop_index(op.f('ix_comment_id'), table_name='comment')
+    op.drop_table('comment')
---
+++ migrations/versions/a4df00aacad7_adding_posts_table.py
@@ -0,0 +1,36 @@
+"""Adding Posts Table
+
+Revision ID: a4df00aacad7
+Revises:
+Create Date: 2022-04-16 22:49:04.121505
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a4df00aacad7'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # create posts table
+    op.create_table(
+        'post',
+        sa.Column('id', sa.Integer(), nullable=False),
+        sa.Column('title', sa.String(length=50), nullable=True),
+        sa.Column('body', sa.String(length=255), nullable=True),
+        sa.Column('owner_id', sa.Integer(), nullable=True),
+        sa.ForeignKeyConstraint(
+            ['owner_id'], ['user.id'], ondelete='CASCADE'),
+        sa.PrimaryKeyConstraint('id')
+    )
+    op.create_index(op.f('ix_post_id'), 'post', ['id'], unique=False)
+
+
+def downgrade():
+    op.drop_index(op.f('ix_post_id'), table_name='post')
+    op.drop_table('post')
