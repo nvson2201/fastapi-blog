186979d97aaaf8cca93e748a01f2607b541729ed
Son Nguyen <n.vanson2201@gmail.com>
2022-04-28 15:47:48 +0700

    add first superusers email and password to settings

--- app/crud/__init__.py
+++ app/crud/__init__.py
@@ -1,6 +1,6 @@
-from .crud_post import post
-from .crud_user import user
-from .crud_comment import comment
+from .crud_post import post  # noqa
+from .crud_user import user  # noqa
+from .crud_comment import comment  # noqa
 # For a new basic set of CRUD operations you could just do
 
 # from .base import CRUDBase
