542e68167a910cfd8ae3936502f216a63cbe21f4
Son Nguyen <n.vanson2201@gmail.com>
2022-04-29 16:50:10 +0700

    resolve conflict pipfile

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,7 @@
 
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import login, users, posts, comments, google  # noqa
+from app.api.api_v1.endpoints import login, users, posts, comments, google
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
