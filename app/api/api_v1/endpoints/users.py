cf02dba96404dc595527f2a0db94985faef5b812
Son Nguyen <n.vanson2201@gmail.com>
2022-05-05 14:21:57 +0700

    use pydantic Object instead of attributes as params

--- app/api/api_v1/endpoints/users.py
+++ app/api/api_v1/endpoints/users.py
@@ -1,8 +1,6 @@
 from typing import Any, List
 
-from fastapi import APIRouter, Body, Depends, HTTPException
-from fastapi.encoders import jsonable_encoder
-from pydantic.networks import EmailStr
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import exc
 from sqlalchemy.orm import Session
 
@@ -60,23 +58,12 @@ def create_user(
 def update_user_me(
     *,
     db: Session = Depends(deps.get_db),
-    password: str = Body(None),
-    full_name: str = Body(None),
-    email: EmailStr = Body(None),
+    user_in: schemas.UserUpdate,
     current_user: models.User = Depends(deps.get_current_active_user),
 ) -> Any:
     """
     Update own user.
     """
-    current_user_data = jsonable_encoder(current_user)
-    user_in = schemas.UserUpdate(**current_user_data)
-    if password is not None:
-        user_in.password = password
-    if full_name is not None:
-        user_in.full_name = full_name
-    if email is not None:
-        user_in.email = email
-
     try:
         user = crud.user.update(db, db_obj=current_user, obj_in=user_in)
     except exc.IntegrityError:
@@ -102,9 +89,7 @@ def read_user_me(
 def create_user_open(
     *,
     db: Session = Depends(deps.get_db),
-    password: str = Body(...),
-    email: EmailStr = Body(...),
-    full_name: str = Body(None),
+    user_in: schemas.UserCreate,
 ) -> Any:
     """
     Create new user without the need to be logged in.
@@ -114,14 +99,12 @@ def create_user_open(
             status_code=403,
             detail="Open user registration is forbidden on this server",
         )
-    user = crud.user.get_by_email(db, email=email)
+    user = crud.user.get_by_email(db, email=user_in.email)
     if user:
         raise HTTPException(
             status_code=409,
             detail="User with this email already exists",
         )
-    user_in = schemas.UserCreate(
-        password=password, email=email, full_name=full_name)
     user = crud.user.create(db, obj_in=user_in)
     return user
 
@@ -142,7 +125,7 @@ def read_user_by_id(
     return user
 
 
-@ router.put("/{user_id}", response_model=schemas.User)
+@router.put("/{user_id}", response_model=schemas.User)
 def update_user(
     *,
     db: Session = Depends(deps.get_db),
@@ -161,7 +144,7 @@ def update_user(
         )
 
     try:
-        user = crud.user.update(db, db_obj=current_user, obj_in=user_in)
+        user = crud.user.update(db, db_obj=user, obj_in=user_in)
     except exc.IntegrityError:
         raise HTTPException(
             status_code=409, detail="User with this email already exists"
